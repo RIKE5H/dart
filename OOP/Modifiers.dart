e395b52eea9167927de39cd1a11cf162e50a3d8a
RIKE5H <rikeshmahat.96@gmail.com>
2022-07-18 08:58:45 +0545

    Acess modifiers

---
+++ OOP/Modifiers.dart
@@ -0,0 +1,27 @@
+// lets learn about the modifiers in dart
+
+// two types
+// Access Modifiers and Non-Acess Modifiers
+
+// => Access Modifiers
+
+/*
+For Class:
+Default Modifiers - everything is in default access modifiers
+private - accessible to current library or files inside the current folders only
+
+For Methods and Properties
+Default - accessible around all folders and libraries
+Private - Accessible only to the current library or, say, for files inside the current folder only.
+ */
+
+// => Non-Access Modifiers
+
+/* 
+For classes
+Abstract - object cannot be created 
+
+For Methods and Properties
+Abstract - 	Can be used in the abstract class and can only be used on methods.
+Static - Belongs to the class rather than the object acts as a single unit for the whole class rather than as an independent unit for each class object.
+*/
